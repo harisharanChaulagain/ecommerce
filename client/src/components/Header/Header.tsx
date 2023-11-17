9ed197106dc58a02d521534764b8e0bf1e90eed6
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-11-17 21:20:16 +0545

    feat: Header section design

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context";
+import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+        <div className="center">Hamro Bazar</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>10</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
