40a0af29e56c656e21e9089381fa0a23f4c99f68
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-11-21 14:34:32 +0545

    feat: Add new category design

--- client/src/components/Category/NewCategory/NewCategory.tsx
+++ client/src/components/Category/NewCategory/NewCategory.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./NewCategory.scss";
+import { MdClose } from "react-icons/md";
+import { Context } from "../../../utils/context";
 
 const NewCategory = () => {
-  console.log("hari shran chaulagain");
-  return <div>New Category</div>;
+  const { setNewCategory }: any = useContext(Context);
+
+  return (
+    <div className="new-category-overlay">
+      <div className="new-category-main">
+        <div className=" main-text">
+          <span>Add New Category</span>
+          <MdClose
+            className="close-icon"
+            onClick={() => setNewCategory(false)}
+          />
+        </div>
+        <input type="text" autoFocus placeholder="Add New Category" />
+        <button className="save-btn">Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default NewCategory;
