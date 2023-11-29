9d08bf207d0a3291a147a75d9690e68278cb0364
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-11-29 21:53:51 +0545

    fix: cart item remove related bux fixed

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,22 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
 import { useProduct } from "../../api/GetApi";
-import { Context } from "../../utils/context";
 
 const Home = () => {
   const { data: productData } = useProduct();
-  const { setProductQuantities, setProductIds, clearCart }: any =
-    useContext(Context);
-
-  useEffect(() => {
-    if (clearCart === true) {
-      setProductQuantities([]);
-      setProductIds([]);
-    }
-  }, [clearCart]);
 
   return (
     <div>
--- client/src/utils/context.tsx
+++ client/src/utils/context.tsx
@@ -9,14 +9,12 @@ import React, {
 interface ContextValue {
   newCategory: boolean;
   newProduct: boolean;
-  clearCart: boolean;
   productQuantities: number[];
   productIds: (string | null)[];
   setNewCategory: Dispatch<SetStateAction<boolean>>;
   setNewProduct: Dispatch<SetStateAction<boolean>>;
   setProductQuantities: Dispatch<SetStateAction<number[]>>;
   setProductIds: Dispatch<SetStateAction<(string | null)[]>>;
-  setClearCart: Dispatch<SetStateAction<boolean>>;
 }
 
 export const Context = createContext<ContextValue | undefined>(undefined);
@@ -30,19 +28,16 @@ export const AppContext: React.FC<AppContextProps> = ({ children }) => {
   const [newProduct, setNewProduct] = useState(false);
   const [productQuantities, setProductQuantities] = useState<number[]>([]);
   const [productIds, setProductIds] = useState<(string | null)[]>([]);
-  const [clearCart, setClearCart] = useState(false);
 
   const contextValue: ContextValue = {
     newCategory,
     newProduct,
     productQuantities,
     productIds,
-    clearCart,
     setNewCategory,
     setNewProduct,
     setProductQuantities,
     setProductIds,
-    setClearCart,
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
