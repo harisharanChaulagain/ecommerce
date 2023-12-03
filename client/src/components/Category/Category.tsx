b7215d308738871769664aeef63dfe8790c48ed4
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-12-03 16:11:16 +0545

    feat:move to particular category of category page from home page category image

--- client/src/components/Category/Category.tsx
+++ client/src/components/Category/Category.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Category.scss";
 import Products from "../Products/Products";
 import { useProduct } from "../../api/GetApi";
+import { useParams } from "react-router-dom";
 
 const Category = () => {
   const { data: productData, isLoading, isError } = useProduct();
+  const { id }: any = useParams();
+  const formattedId = id.replace(/\s/g, "-");
+
+  useEffect(() => {
+    const scrollToCategory = document.getElementById(formattedId);
+    if (scrollToCategory) {
+      scrollToCategory.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [formattedId]);
 
   if (isLoading) {
     return <div>Loading products...</div>;
@@ -20,7 +30,7 @@ const Category = () => {
     <div className="category-main-content">
       <div className="layout">
         {Object.entries(groupedProducts).map(([category, products]) => (
-          <div key={category} id={category}>
+          <div key={category} id={`${`${category.replace(/\s/g, "-")}`}`}>
             <div className="category-title">{category}</div>
             <Products
               headingText={category}
