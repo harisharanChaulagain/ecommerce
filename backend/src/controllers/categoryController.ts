465971f48d7dda8fb5e75b48a97a958e9b225212
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2024-01-14 10:38:12 +0545

    fix: image update in category

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -80,30 +80,48 @@ export const deleteCategoryById = async (req: Request, res: Response) => {
 export const updateCategory = async (req: Request, res: Response) => {
   try {
     const categoryId = req.params.id;
-    const updates = req.body;
-
     if (!categoryId) {
-      return res.status(400).json({
-        error: "Category id is required",
-      });
+      return res.status(400).json({ error: "Category id is required" });
     }
 
-    const category = await Category.findById(categoryId);
-    if (!category) {
-      return res.status(404).json({ error: "Category not found." });
+    const existingCategory = await Category.findById(categoryId);
+    if (!existingCategory) {
+      return res.status(404).json({ error: "Category not found" });
     }
-    for (const key in updates) {
-      if (Object.prototype.hasOwnProperty.call(updates, key)) {
-        (category as any)[key] = updates[key];
+
+    const categoryUpdate: any = {};
+    if (req.body.name) {
+      categoryUpdate.name = req.body.name;
+    }
+
+    if (req.files && req.files.image) {
+      const imageFile = req.files.image as UploadedFile;
+
+      if (!isValidImageType(imageFile.mimetype)) {
+        return res.status(400).json({
+          error: "Invalid file type. Please upload a JPEG or PNG image.",
+        });
       }
+
+      categoryUpdate.image = imageFile.data;
     }
-    await category.save();
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      { $set: categoryUpdate },
+      { new: true }
+    );
+
+    if (!updatedCategory) {
+      return res.status(500).json({ error: "Failed to update category" });
+    }
+
     res.status(200).json({
       message: "Category updated successfully!!",
-      updatedcategory: category,
+      updatedCategory,
     });
   } catch (error) {
-    console.error("Error updating products:", error);
+    console.error("Error updating category:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
