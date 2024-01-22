3c1e1462b625c111a3863957a96918ceaee617b7
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2024-01-22 07:19:18 +0545

    feat:download invoice as pdf file

--- client/src/components/Invoice/Invoice.tsx
+++ client/src/components/Invoice/Invoice.tsx
@@ -1,5 +1,7 @@
+import html2canvas from "html2canvas";
 import Cookies from "js-cookie";
-import React from "react";
+import jsPDF from "jspdf";
+import React, { useState } from "react";
 import { FaArrowDown } from "react-icons/fa";
 import { useCompanyDetails, useUsersDetails } from "../../api/GetApi";
 import { bufferToDataURL } from "../../utils/imageUtils";
@@ -11,6 +13,7 @@ import "./Invoice.scss";
 import InvoiceTable from "./InvoiceTable/InvoiceTable";
 
 const Invoice = () => {
+  const [loader, setLoader] = useState(false);
   const { data: companyData } = useCompanyDetails();
   const { data: userData } = useUsersDetails();
 
@@ -24,19 +27,32 @@ const Invoice = () => {
   const currentYear = currentDate.getFullYear();
   const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, "0");
   const currentDay = currentDate.getDate().toString().padStart(2, "0");
-
   const formattedDate = `${currentYear}/${currentMonth}/${currentDay}`;
 
+  const downloadPDF = () => {
+    const capture = document.querySelector(".invoice-main") as HTMLElement;
+    setLoader(true);
+    html2canvas(capture).then((canvas) => {
+      const imgData = canvas.toDataURL("img/png");
+      const doc = new jsPDF("p", "mm", "a4");
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      doc.addImage(imgData, "PNG", 0, 0, componentWidth, componentHeight);
+      setLoader(false);
+      doc.save("receipt.pdf");
+    });
+  };
+
   return (
-    <div className="invoice-main">
-      {companyData?.map((detail: companyDetails) => (
-        <>
-          <span className="download-btn">
-            <div className="download-btn-content">
-              <FaArrowDown />
-              Download
-            </div>
-          </span>
+    <>
+      <span className="download-btn" onClick={downloadPDF}>
+        <button className="download-btn-content" disabled={loader}>
+          <FaArrowDown />
+          {loader ? <span>Downloading</span> : <span>Download </span>}
+        </button>
+      </span>
+      <div className="invoice-main">
+        {companyData?.map((detail: companyDetails) => (
           <div className="company-details">
             <span className="logo">
               {detail?.logo?.data && (
@@ -60,41 +76,41 @@ const Invoice = () => {
               </div>
             </div>
           </div>
-        </>
-      ))}
-      {filteredUserData?.map((detail: UserDetails) => (
-        <div className="invoice-details">
-          <div className="first">Invoice Details</div>
-          <hr />
-          <div className="second">
-            <div className="date">
-              <div>
-                Date:
-                <span> {formattedDate}</span>
+        ))}
+        {filteredUserData?.map((detail: UserDetails) => (
+          <div className="invoice-details">
+            <div className="first">Invoice Details</div>
+            <hr />
+            <div className="second">
+              <div className="date">
+                <div>
+                  Date:
+                  <span> {formattedDate}</span>
+                </div>
+                <div>
+                  Invoice No:.
+                  <span># fkd443RFdf</span>
+                </div>
               </div>
               <div>
-                Invoice No:.
-                <span># fkd443RFdf</span>
+                Customer Name: <span>{detail?.fullName}</span>
               </div>
-            </div>
-            <div>
-              Customer Name: <span>{detail?.fullName}</span>
             </div>
           </div>
+        ))}
+        <div className="particulars">
+          <InvoiceTable />
         </div>
-      ))}
-      <div className="particulars">
-        <InvoiceTable />
-      </div>
-      <div className="footer">
-        <div className="first">
-          <span> Total: </span>
-          <span>&#8377; 4354</span>
+        <div className="footer">
+          <div className="first">
+            <span> Total: </span>
+            <span>&#8377; 4354</span>
+          </div>
+          <hr />
+          <div className="second">Thank You!</div>
         </div>
-        <hr />
-        <div className="second">Thank You!</div>
       </div>
-    </div>
+    </>
   );
 };
 
