7930d25bc136c1e3608a9c0ea4f52d7e7aab80e5
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2024-07-19 16:46:21 +0545

    baseurl updated

--- client/src/lib/Axios.ts
+++ client/src/lib/Axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "http://localhost:3001",
+  // baseURL: "http://localhost:3001",
+  baseURL: "https://backend-888s.onrender.com",
 });
